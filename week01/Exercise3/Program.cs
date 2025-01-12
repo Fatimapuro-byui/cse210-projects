24a94a13c62a1133e2c5970bd3004f75859ac41f
Fatima Puro <122589022+Fatimapuro-byui@users.noreply.github.com>
2025-01-12 00:33:10 -0400

    Update Program.cs

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,64 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        
+
+
+
+
+    
+        
+        while (true)
+        {
+            
+            Random rand = new Random();
+            int magicNumber = rand.Next(1, 101);  
+            
+            
+            int userGuess = -1;
+            int guessCount = 0;
+
+            
+            Console.WriteLine("Welcome to Guess My Number!");
+            Console.WriteLine("I have picked a magic number between 1 and 100.");
+            Console.WriteLine("Try to guess the number!");
+
+            
+            while (userGuess != magicNumber)
+            {
+                
+                Console.Write("What is your guess? ");
+                userGuess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                
+                if (userGuess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (userGuess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {guessCount} guesses.");
+                }
+            }
+
+            
+            Console.Write("Do you want to play again? (yes/no): ");
+            string playAgain = Console.ReadLine().ToLower();
+
+            if (playAgain != "yes")
+            {
+                Console.WriteLine("Thanks for playing! Goodbye.");
+                break;  
+            }
+        }
     }
 }
+
+    
