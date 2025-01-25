75b379c7aca738d05a371ddc1e4ece2ea83d1df3
Fatima Puro <122589022+Fatimapuro-byui@users.noreply.github.com>
2025-01-25 14:32:38 -0400

    Update Reference.cs

--- week03/ScriptureMemorizer/Reference.cs
+++ week03/ScriptureMemorizer/Reference.cs
@@ -1,1 +1,42 @@
+using System;
 
+public class Reference
+{
+    public string Book { get; private set; }
+    public int Chapter { get; private set; }
+    public int StartVerse { get; private set; }
+    public int EndVerse { get; private set; }
+    public bool HasMultipleVerses { get; private set; }
+
+    
+    public Reference(string book, int chapter, int verse)
+    {
+        Book = book;
+        Chapter = chapter;
+        StartVerse = verse;
+        EndVerse = verse;
+        HasMultipleVerses = false;
+    }
+
+    
+    public Reference(string book, int chapter, int startVerse, int endVerse)
+    {
+        Book = book;
+        Chapter = chapter;
+        StartVerse = startVerse;
+        EndVerse = endVerse;
+        HasMultipleVerses = true;
+    }
+
+    public override string ToString()
+    {
+        if (HasMultipleVerses)
+        {
+            return $"{Book} {Chapter}:{StartVerse}-{EndVerse}";
+        }
+        else
+        {
+            return $"{Book} {Chapter}:{StartVerse}";
+        }
+    }
+}
