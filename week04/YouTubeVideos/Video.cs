eec675ad5655486c0f1a31b4d86a8788e93579c3
Fatima Puro <122589022+Fatimapuro-byui@users.noreply.github.com>
2025-01-28 22:33:27 -0400

    Update Video.cs

--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -1,1 +1,23 @@
+namespace YouTubeVideos
+{
+    public class Video
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Length { get; set; }
+        public List<Comment> Comments { get; set; }
 
+        public Video(string title, string author, int length)
+        {
+            Title = title;
+            Author = author;
+            Length = length;
+            Comments = new List<Comment>();
+        }
+
+        public int GetNumberOfComments()
+        {
+            return Comments.Count;
+        }
+    }
+}
