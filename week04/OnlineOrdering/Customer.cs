429e341cc0c5546ce863fb598ed7d2fd11f30672
Fatima Puro <122589022+Fatimapuro-byui@users.noreply.github.com>
2025-01-31 13:28:13 -0400

    Update Customer.cs

--- week04/OnlineOrdering/Customer.cs
+++ week04/OnlineOrdering/Customer.cs
@@ -1,1 +1,22 @@
+public class Customer
+{
+    private string name;
+    private Address address;
+
+    public Customer(string name, Address address)
+    {
+        this.name = name;
+        this.address = address;
+    }
+
+    public bool IsInUSA()
+    {
+        return address.IsInUSA();
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"{name}\n{address.GetFullAddress()}";
+    }
+}
 
