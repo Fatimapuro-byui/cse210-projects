57d060f05421a40101b2f9a5b635239ad37ac991
Fatima Puro <122589022+Fatimapuro-byui@users.noreply.github.com>
2025-01-31 13:29:03 -0400

    Update Order.cs

--- week04/OnlineOrdering/Order.cs
+++ week04/OnlineOrdering/Order.cs
@@ -1,1 +1,50 @@
+using System;
+using System.Collections.Generic;
+
+public class Order
+{
+    private List<Product> products;
+    private Customer customer;
+    private const double USA_SHIPPING_COST = 5.00;
+    private const double INTERNATIONAL_SHIPPING_COST = 35.00;
+
+    public Order(Customer customer)
+    {
+        products = new List<Product>();
+        this.customer = customer;
+    }
+
+    public void AddProduct(Product product)
+    {
+        products.Add(product);
+    }
+
+    public double GetTotalCost()
+    {
+        double totalProductCost = 0;
+
+        foreach (var product in products)
+        {
+            totalProductCost += product.GetTotalCost();
+        }
+
+        double shippingCost = customer.IsInUSA() ? USA_SHIPPING_COST : INTERNATIONAL_SHIPPING_COST;
+        return totalProductCost + shippingCost;
+    }
+
+    public string GetPackingLabel()
+    {
+        string label = "Packing Label:\n";
+        foreach (var product in products)
+        {
+            label += product.GetProductLabel() + "\n";
+        }
+        return label;
+    }
+
+    public string GetShippingLabel()
+    {
+        return customer.GetShippingLabel();
+    }
+}
 
