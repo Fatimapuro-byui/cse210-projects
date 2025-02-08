d203e3b0a94a466839a662d191308672779f03f7
Fatima Puro <122589022+Fatimapuro-byui@users.noreply.github.com>
2025-02-08 03:31:29 -0400

    Update MindfulnessActivity.cs

--- week05/Mindfulness/MindfulnessActivity.cs
+++ week05/Mindfulness/MindfulnessActivity.cs
@@ -1,1 +1,28 @@
+using System;
+using System.Collections.Generic;
 
+public abstract class MindfulnessActivity
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Duration { get; set; }
+
+    public virtual void Start()
+    {
+        Console.WriteLine($"Starting {Name} activity...");
+        Console.WriteLine(Description);
+        Console.Write("Enter the duration (in seconds): ");
+        Duration = int.Parse(Console.ReadLine());
+        Console.WriteLine("Prepare to begin...");
+        System.Threading.Thread.Sleep(3000); 
+    }
+
+    public virtual void End()
+    {
+        Console.WriteLine("Good job! You have completed the activity.");
+        Console.WriteLine($"Activity: {Name}, Duration: {Duration} seconds");
+        System.Threading.Thread.Sleep(3000);
+    }
+
+    public abstract void PerformActivity();
+}
