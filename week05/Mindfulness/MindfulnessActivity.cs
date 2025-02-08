60bd5bf130183aae79d19433cfaca9967bdbd1d7
Fatima Puro <122589022+Fatimapuro-byui@users.noreply.github.com>
2025-02-08 08:26:18 -0400

    Update MindfulnessActivity.cs

--- week05/Mindfulness/MindfulnessActivity.cs
+++ week05/Mindfulness/MindfulnessActivity.cs
@@ -1,12 +1,11 @@
-using System;
-using System.Collections.Generic;
-
 public abstract class MindfulnessActivity
 {
+    
     public string Name { get; set; }
     public string Description { get; set; }
     public int Duration { get; set; }
 
+    
     public virtual void Start()
     {
         Console.WriteLine($"Starting {Name} activity...");
@@ -14,15 +13,35 @@ public abstract class MindfulnessActivity
         Console.Write("Enter the duration (in seconds): ");
         Duration = int.Parse(Console.ReadLine());
         Console.WriteLine("Prepare to begin...");
-        System.Threading.Thread.Sleep(3000); 
+        ShowSpinner(3); 
     }
 
+    
     public virtual void End()
     {
         Console.WriteLine("Good job! You have completed the activity.");
         Console.WriteLine($"Activity: {Name}, Duration: {Duration} seconds");
-        System.Threading.Thread.Sleep(3000);
+        ShowSpinner(3); 
     }
 
+    
     public abstract void PerformActivity();
+
+    
+    protected void ShowSpinner(int duration)
+    {
+        var spinnerChars = new[] { '/', '-', '\\', '|' };
+        var originalX = Console.CursorLeft;
+        var originalY = Console.CursorTop;
+
+        for (var i = 0; i < duration * 10; i++)
+        {
+            Console.SetCursorPosition(originalX, originalY);
+            Console.Write(spinnerChars[i % spinnerChars.Length]);
+            System.Threading.Thread.Sleep(100);
+        }
+
+        Console.SetCursorPosition(originalX, originalY);
+        Console.Write(' '); 
+    }
 }
