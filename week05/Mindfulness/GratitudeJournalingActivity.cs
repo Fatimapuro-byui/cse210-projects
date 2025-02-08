23af3329e094551e5a9a08d2ec4af272a2ca6aae
Fatima Puro <122589022+Fatimapuro-byui@users.noreply.github.com>
2025-02-08 03:29:19 -0400

    Update GratitudeJournalingActivity.cs

--- week05/Mindfulness/GratitudeJournalingActivity.cs
+++ week05/Mindfulness/GratitudeJournalingActivity.cs
@@ -1,1 +1,20 @@
+public class GratitudeJournalingActivity : MindfulnessActivity
+{
+    public GratitudeJournalingActivity()
+    {
+        Name = "Gratitude Journaling";
+        Description = "This activity will help you reflect on the things you are grateful for by having you write them down.";
+    }
 
+    public override void PerformActivity()
+    {
+        Console.WriteLine("Start writing down things you are grateful for:");
+        DateTime endTime = DateTime.Now.AddSeconds(Duration);
+        while (DateTime.Now < endTime)
+        {
+            string item = Console.ReadLine();
+            if (string.IsNullOrEmpty(item)) break;
+            Console.WriteLine($"You wrote: {item}");
+        }
+    }
+}
