495f77ff14185d8db136e7479a2512ea7db3d120
Fatima Puro <122589022+Fatimapuro-byui@users.noreply.github.com>
2025-02-16 17:42:38 -0400

    Update Goal.cs

--- week06/EternalQuest/Goal.cs
+++ week06/EternalQuest/Goal.cs
@@ -1,1 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
 
+
+
+
+public abstract class Goal
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Points { get; set; }
+    public bool IsComplete { get; set; }
+    public DateTime? DateLastCompleted { get; set; } = null; 
+
+    public Goal(string name, string description, int points)
+    {
+        Name = name;
+        Description = description;
+        Points = points;
+        IsComplete = false;
+    }
+
+    public abstract int RecordEvent();
+    public abstract string GetDetailsString(); 
+    public abstract string GetStringRepresentation(); 
+    public virtual string GetCompletionStatus()
+    {
+        return IsComplete ? "[X]" : "[ ]";
+    }
+}
