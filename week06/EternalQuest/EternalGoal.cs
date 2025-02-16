1dd3db6f6367c648eddb39c1530c8913faae59a1
Fatima Puro <122589022+Fatimapuro-byui@users.noreply.github.com>
2025-02-16 17:28:36 -0400

    Update EternalGoal.cs

--- week06/EternalQuest/EternalGoal.cs
+++ week06/EternalQuest/EternalGoal.cs
@@ -1,1 +1,20 @@
+public class EternalGoal : Goal
+{
+    public EternalGoal(string name, string description, int points) : base(name, description, points) { }
 
+    public override int RecordEvent()
+    {
+        DateLastCompleted = DateTime.Now;
+        return Points;
+    }
+
+    public override string GetDetailsString()
+    {
+        return $"{GetCompletionStatus()} {Name} ({Description})";
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"EternalGoal:{Name},{Description},{Points},{IsComplete},{DateLastCompleted?.ToString("o")}";
+    }
+}
