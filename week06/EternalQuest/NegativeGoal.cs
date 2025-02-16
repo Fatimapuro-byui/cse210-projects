9b1f81751f66c1f4f7ca4a3cd45815f456345491
Fatima Puro <122589022+Fatimapuro-byui@users.noreply.github.com>
2025-02-16 17:30:53 -0400

    Update NegativeGoal.cs

--- week06/EternalQuest/NegativeGoal.cs
+++ week06/EternalQuest/NegativeGoal.cs
@@ -1,1 +1,20 @@
+public class NegativeGoal : Goal
+{
+    public NegativeGoal(string name, string description, int points) : base(name, description, points) { }
 
+    public override int RecordEvent()
+    {
+        DateLastCompleted = DateTime.Now;
+        return -Points; 
+    }
+
+    public override string GetDetailsString()
+    {
+        return $"{GetCompletionStatus()} {Name} ({Description})";
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"NegativeGoal:{Name},{Description},{Points},{IsComplete},{DateLastCompleted?.ToString("o")}";
+    }
+}
