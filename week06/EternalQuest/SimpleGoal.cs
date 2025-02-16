04ad362bf9273d10d8f85dd790e6c18b583a192a
Fatima Puro <122589022+Fatimapuro-byui@users.noreply.github.com>
2025-02-16 17:48:02 -0400

    Update SimpleGoal.cs

--- week06/EternalQuest/SimpleGoal.cs
+++ week06/EternalQuest/SimpleGoal.cs
@@ -1,1 +1,25 @@
+public class SimpleGoal : Goal
+{
+    public SimpleGoal(string name, string description, int points) : base(name, description, points) { }
 
+    public override int RecordEvent()
+    {
+        if (!IsComplete)
+        {
+            IsComplete = true;
+            DateLastCompleted = DateTime.Now;
+            return Points;
+        }
+        return 0; 
+    }
+
+    public override string GetDetailsString()
+    {
+        return $"{GetCompletionStatus()} {Name} ({Description})";
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"SimpleGoal:{Name},{Description},{Points},{IsComplete},{DateLastCompleted?.ToString("o")}";
+    }
+}
