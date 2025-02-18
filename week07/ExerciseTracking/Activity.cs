84d76ba37e8ae0bfab91f15f8fa71aaa0d653a78
Fatima Puro <122589022+Fatimapuro-byui@users.noreply.github.com>
2025-02-17 21:58:34 -0400

    Update Activity.cs

--- week07/ExerciseTracking/Activity.cs
+++ week07/ExerciseTracking/Activity.cs
@@ -1,1 +1,36 @@
+using System;
 
+public class Activity
+{
+    private DateTime date;
+    private int lengthInMinutes; 
+
+    public Activity(DateTime date, int lengthInMinutes)
+    {
+        this.date = date;
+        this.lengthInMinutes = lengthInMinutes;
+    }
+
+    public DateTime Date => date;
+    public int LengthInMinutes => lengthInMinutes;
+
+    public virtual double GetDistance()
+    {
+        return 0; 
+    }
+
+    public virtual double GetSpeed()
+    {
+        return (GetDistance() / lengthInMinutes) * 60; 
+    }
+
+    public virtual double GetPace()
+    {
+        return lengthInMinutes / GetDistance(); 
+    }
+
+    public virtual string GetSummary()
+    {
+        return $"{date.ToShortDateString()} {GetType().Name} ({lengthInMinutes} min) - Distance: {GetDistance():F1} miles, Speed: {GetSpeed():F1} mph, Pace: {GetPace():F1} min per mile";
+    }
+}
