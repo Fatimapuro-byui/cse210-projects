536d06ecf5abbdb52bb604999744e82ccf5a7a5b
Fatima Puro <122589022+Fatimapuro-byui@users.noreply.github.com>
2025-02-18 00:54:25 -0400

    Update Cycling.cs

--- week07/ExerciseTracking/Cycling.cs
+++ week07/ExerciseTracking/Cycling.cs
@@ -1,1 +1,27 @@
+public class Cycling : Activity
+{
+    private double speed; 
 
+    
+    public Cycling(DateTime date, int lengthInMinutes, double speed)
+        : base(date, lengthInMinutes)
+    {
+        this.speed = speed;
+    }
+
+    
+    public override double GetDistance()
+    {
+        return (speed * LengthInMinutes) / 60; 
+    }
+
+    public override double GetSpeed()
+    {
+        return speed; 
+    }
+
+    public override double GetPace()
+    {
+        return 60 / speed; 
+    }
+}
