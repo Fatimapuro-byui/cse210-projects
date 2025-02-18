8f1a722177ea6b98a6348f7c7de852cc2ea9df37
Fatima Puro <122589022+Fatimapuro-byui@users.noreply.github.com>
2025-02-18 01:14:10 -0400

    Update Program.cs

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,1 +1,22 @@
+using System;
+using System.Collections.Generic;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(new DateTime(2024, 11, 3), 45, 5.0),
+            new Cycling(new DateTime(2024, 11, 4), 45, 15.0),
+            new Swimming(new DateTime(2024, 11, 5), 45, 30)
+        };
+
+        
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
+    }
+}
