25f54be8da2dbf52f6d4970ab605b967e7bb4a90
Fatima Puro <122589022+Fatimapuro-byui@users.noreply.github.com>
2025-02-17 22:17:29 -0400

    Update Running.cs

--- week07/ExerciseTracking/Running.cs
+++ week07/ExerciseTracking/Running.cs
@@ -1,1 +1,15 @@
+public class Running : Activity
+{
+    private double distance; 
 
+    public Running(DateTime date, int lengthInMinutes, double distance)
+        : base(date, lengthInMinutes)
+    {
+        this.distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return distance;
+    }
+}
